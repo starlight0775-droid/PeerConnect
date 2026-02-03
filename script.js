564dc1a5c70553f62c53068a10694f60860e0cc7
starlight0775-droid <starlight0775@gmail.com>
2026-02-03 15:15:48 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const reveals = document.querySelectorAll(".reveal");
+
+function revealOnScroll() {
+  reveals.forEach(el => {
+    const windowHeight = window.innerHeight;
+    const elementTop = el.getBoundingClientRect().top;
+
+    if (elementTop < windowHeight - 100) {
+      el.classList.add("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+revealOnScroll();
